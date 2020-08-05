15a4de63ca774b9ad47ac0d158a78d3a6220af7e
lvv5040 <66978068+lvv5040@users.noreply.github.com>
2020-08-05 08:18:52 -0600

    updated

--- script.js
+++ script.js
@@ -1,88 +1,39 @@
-function realTime() {
-    var dateTime = moment().format("LLLL");
-    console.log(dateTime);
-    $("#currentDay").append(dateTime);
-  }
-  realTime();
-  console.log(realTime);
-  
-  var timeHour;
-  var hour;
-  
-  
-  
-  $("button:submit").on("click", function() {
+var textboxArray = $(".form-control");
+textboxArray.forEach(function(myElement){{
+          if ( this.moment === "ago" ) {
+            this.style.color = ".past";
+          } 
+            else if (this.moment === "from now") {
+                this.style.color = ".future";
+                }
+                else {
+                    this.style.color = ".present";
+                }
+            
+        };
+      });
 
-    var userInput = $(this)
-      .closest("tr")
-      .find("input:text")
-      .val();
-  
-    var hour = $(this).data("hour");
-  
-    timeHour = $(this).attr("data-hour");
-  
-    localStorage.setItem(timeHour, userInput);
-  
-    console.log(userInput);
-    console.log(hour);
-  });
-  
-  var storageVariable = localStorage.getItem("userArray");
-  storageVariable = JSON.parse(storageVariable);
-  
-  if (!storageVariable) {
-    storageVariable = [];
+
+
+var textboxArray = $(".form-control");
+for (let i = 0; i < textboxArray.length; i++) {
+    let val = getCookie("box" + i);
+    textboxArray[i].value = val != null ? val : "";
   }
-  console.log("this is a log" + storageVariable);
-  
-  //This function isnt working for me
-  function displaySavedText() {
-    for (var i = 0; i < storageVariable.length; i++) {
-      var savedText = storageVariable[i].text;
-      console.log("this is text:", savedText);
-    }
+   
+  document.getElementById("date").innerHTML = moment();
+   
+  console.log(moment())
+  function test(box) {
+    setCookie("box" + box, textboxArray[box].value);
   }
-  displaySavedText();
-  
-  
-  var currentTime = moment().format("H");
-  console.log(currentTime);
-  
-  
-  var trArray = $(".hour").get();
-  
-  for (var i = 0; i < trArray.length; i++) {
-    var trId = parseInt(trArray[i].id);
-    if (trId < parseInt(currentTime)) {
-      console.log("this is trid: ", trId);
-      trArray[i].classList.add("past");
-      console.log(trArray[i]);
-  
-      var savedHour = trArray[i].id;
-      var savedText = localStorage.getItem(hour);
-      trArray[i].val = savedText;
-      console.log("this is hour: ", savedHour);
-      console.log(savedText);
-    } else if (trId > parseInt(currentTime)) {
-      console.log("this is trid: ", trId);
-      trArray[i].classList.add("future");
-      console.log(trArray[i]);
-  
-      var savedHour = trArray[i].id;
-      var savedText = localStorage.getItem(hour);
-      trArray[i].val = savedText;
-      console.log("this is hour: ", savedHour);
-      console.log(savedText);
-    } else {
-      trArray[i].classList.add("present");
-      console.log(trArray[i]);
-  
-      var savedHour = trArray[i].id;
-      var savedText = localStorage.getItem(hour);
-      trArray[i].val = savedText;
-      console.log("this is hour: ", savedHour);
-      console.log(savedText);
-    }
+
+  function setCookie(name, value) {
+    document.cookie = name + "=" + value + ";path=/;";
+
   }
-  
+   
+  function getCookie(name) {
+    var v = document.cookie.match("(^|;) ?" + name + "=([^;]*)(;|$)");
+    return v ? v[2] : null;
+  }
